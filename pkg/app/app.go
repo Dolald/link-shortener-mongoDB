f9bc495f1050ed4eb0724ee66c1e135fed00cf5e
Alex Bazhenov <vorobushec13@gmail.com>
2024-06-27 19:23:00 +0300

    create configs and add its

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,25 +2,55 @@ package app
 
 import (
 	"context"
+	"log"
+	"shortener/pkg/repository"
+	"time"
 
-	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Run(ctx context.Context) error {
-	// connect to our mongoDB
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
-	if err != nil {
-		logrus.Error("an error has occurred")
+
+	if err := configsInit(); err != nil {
 		return err
 	}
 
+	clientOptions := options.Client().ApplyURI("mongodb://root:qwerty@localhost:27017").SetAuth(options.Credential{
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Подключение к MongoDB
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Отключение от MongoDB при завершении работы
 	defer func() {
 		if err := client.Disconnect(ctx); err != nil {
-			logrus.Error(err)
+			log.Fatal(err)
 		}
 	}()
 
-	return nil
+	// Создание экземпляра UrlDAO
+	_, err = repository.NewUrlDAO(ctx, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("The database was created and indices were set up successfully")
+
+	return err
+}
+
+func configsInit() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
